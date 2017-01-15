2343910b71db4d5afc6bbf430e3fc6f259f1a0ff
xinleiye <xinlei_ye@163.com>
2017-01-15 11:00:20 +0800

    /* 名称：短语随机生成器 功能：     1: 点击一个按钮就生存一个新的随机引用短语。     2: 点击一个按钮就把这个引用短语发到twitter中。 author：yexinlei，2016.12.10 */

---
+++ FreeCodeCamp/Build a Random Quote Machine/home.js
@@ -0,0 +1,84 @@
+/*
+code by yexinlei
+*/
+var colors = ['#16a085', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#fb6964', '#342224', '#472e32', '#bdbb99', '#77b1a9', '#73a857'];
+var currentQuote = '';
+var currentAuthor = '';
+
+function inIframe() {
+  try {
+    return window.self  !== window.top;
+  }
+  catch(e) {
+    return true;
+  }
+}
+
+function openURL(url) {
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=0');
+}
+
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(response) {
+      var r = JSON.parse(response);
+      currentQuote = r.quote;
+      currentAuthor = r.author;
+      if (inIframe()) {
+        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '"' + currentAuthor));
+        $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' + encodeURIComponent(currentAuthor) + '&content=' + encodeURIComponent(currentQuote));
+      }
+
+      $(".quote-text").animate({
+        opacity: 0
+      }, 500,
+      function() {
+        $(this).animate({
+          opacity: 1
+        }, 500);
+        $('#text').text(r.quote);
+      });
+
+      $(".quote-author").animate({
+        opacity: 0
+      }, 500,
+      function(){
+        $(this).animate({
+          opacity: 1
+        }, 500);
+        $('#author').html(r.author);
+      });
+
+      var color = Math.floor(Math.random() * colors.length);
+      $("html body").animate({
+        backgroundColor: colors[color],
+        color: colors[color]
+      }, 1000);
+      $(".button").animate({
+        backgroundColor: colors[color]
+      }, 1000);
+    }
+  });
+}
+
+$(document).ready(function(){
+  getQuote();
+  $('#new-quote').on('click', getQuote);
+  $('#tweet-quote').on('click', function() {
+    if (!inIframe()) {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '"' + currentAuthor));
+    }
+  });
+
+  $('#tumblr-quote').on('click', function() {
+    if (!inIframe()) {
+      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' + encodeURIComponent(currentAuthor) + '&content=' + encodeURIComponent(currentQuote));
+    }
+  });
+});
