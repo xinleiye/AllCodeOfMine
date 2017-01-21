d0a2b0752d9ce7a8b0a29068c417f1b537b75c69
xinleiye <xinlei_ye@163.com>
2017-01-21 23:07:00 +0800

    angular.js基础 1.修改控制器的一个问题，时间可以正常显示出来

--- Angular/js/app.js
+++ Angular/js/app.js
@@ -1,12 +1,15 @@
-function MyController($scope) {
-$scope.clock = {
-now: new Date()
-};
-var updateClock = function() {
-$scope.clock.now = new Date()
-};
-setInterval(function() {
-$scope.$apply(updateClock);
-}, 1000);
-updateClock();
-};
+var appClock = angular.module("appClock", [])
+
+appClock.controller("MyController", function($scope) {
+    $scope.clock = {
+        now: new Date()
+    };
+    var updateClock = function() {
+        $scope.clock.now = new Date()
+    };
+    setInterval(function() {
+        $scope.$apply(updateClock);
+    }, 1000);
+    updateClock();
+});
+
