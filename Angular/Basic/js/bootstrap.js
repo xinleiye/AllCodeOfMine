511177009ab7dff02587fcc136d900f50ecceb39
xinleiye <xinlei_ye@163.com>
2017-02-10 23:49:03 +0800

    Angular自学代码 1.完成了一个与Bootstrap有关的应用，该应用有很大的优化空间

--- Angular/Basic/js/bootstrap.js
+++ Angular/Basic/js/bootstrap.js
@@ -1,6 +1,10 @@
 var app = angular.module("myApp", []);
 
 app.controller("userController", function($scope){
+    $scope.fName = '';
+    $scope.lName = '';
+    $scope.passWord1 = '';
+    $scope.passWord2 = '';
     $scope.users = [
         {id:1, fName:'Hege', lName:"Pege" },
         {id:2, fName:'Kim',  lName:"Pim" },
@@ -9,7 +13,46 @@ app.controller("userController", function($scope){
         {id:5, fName:'John', lName:"Doe" },
         {id:6, fName:'Peter',lName:"Pan" }
     ];
-    $scope.editUser = function(id){
+    $scope.edit = true;
+    $scope.error = false;
+    $scope.incomplete = false;
 
+    $scope.editUser = function(id){
+        if (id === 'new') {
+            $scope.edit = true;
+            $scope.incomplete = true;
+            $scope.fName = '';
+            $scope.lName = '';
+        } else {
+            $scope.edit = false;
+            $scope.fName = $scope.users[id - 1].fName;
+            $scope.lName = $scope.users[id - 1].lName;
+        }
+    };
+
+    $scope.$watch('passWord1', function() {
+        $scope.test();
+    });
+    $scope.$watch('passWord2', function() {
+        $scope.test();
+    });
+    $scope.$watch('fName', function() {
+        $scope.test();
+    });
+    $scope.$watch('lName', function() {
+        $scope.test();
+    });
+
+    $scope.test = function() {
+        if ($scope.passWord1 !== $scope.passWord2) {
+            $scope.error = true;
+        } else {
+            $scope.error = false;
+        }
+        $scope.incomplete = false;
+        if ($scope.edit && 
+           (!$scope.fName.length || !$scope.lName.length || !$scope.passWord1.length || !$scope.passWord2.length)) {
+            $scope.incomplete = true;
+        }
     };
 });
