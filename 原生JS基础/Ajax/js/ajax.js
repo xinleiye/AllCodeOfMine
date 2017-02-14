e443fb48210df4671fe3498abbd667a094d0566b
xinleiye <xinlei_ye@163.com>
2017-02-14 23:46:09 +0800

    原生JS基础--Ajax 1.编码完成一个简单的Ajax测试程序 2.程序未调试通，可能原因：node建立的本地服务器依赖包未装全，无法找到Ajax请求的文件

--- 原生JS基础/Ajax/js/ajax.js
+++ 原生JS基础/Ajax/js/ajax.js
@@ -6,7 +6,7 @@
 4.接收响应数据
 */
 
-function ajax() {
+function loadXMLDoc(url) {
     //1.实例化XMLHttpRequest对象
     var httpObj = {};
 
@@ -16,11 +16,29 @@ function ajax() {
         httpObj = new ActiveXObject("Microsoft.XMLHTTP");
     }
 
-    //2.连接服务器
-    httpObj.open(method, url, async);
+    /*
+      2.连接服务器
+        1).method:请求类型GET POST
+        2).url:文件在服务器上的位置
+        3).async:true异步，false同步
+    */
+    //httpObj.open("GET", url, true);
 
-    //3.发送请求
-    httpObj.send(string);
+    /*
+      使用POST时，添加HTTP头
+        1).header:头的名称
+        2).value:头的值
+    */
+    //httpObj.setRequestHeader(header, value);
+
+    /*
+      3.发送请求
+        1).string:只用于POST请求，GET请求时为空
+        2).GET与POST的区别
+          GET:简单，块
+          POST:无法缓存；数据量大；可发送包含未知字符的用户输入
+    */
+    //httpObj.send(string);
 
     /*
       4.接收响应数据
@@ -31,7 +49,7 @@ function ajax() {
           3).请求处理中
           4).请求已完成，且响应已就绪
         请求的结果保存在status中：
-          1).200~300:"OK"
+          1).[200,300):"OK"
           2).404:页面未找到
           3).其他：错误
         每当readState发生改变时，会触发onreadystatechange事件
@@ -40,12 +58,14 @@ function ajax() {
           2).responseXML:获得XML形式的响应数据
     */
     httpObj.onreadystatechange = function() {
-        if (httpObj.readyState === 4 && httpObj.status === 200) {
+        if (httpObj.readyState === 4) {
+            if ( httpObj.status >= 200 && httpObj.status < 300) {
+                document.getElementById("test").innerHTML=httpObj.responseText;
+            } else {
             //dosomething
-        } else if () {
-            //dosomething
-        } else {
-            //dosomething
+            }
         }
-    }
+    };
+    httpObj.open("GET", url, true);
+    httpObj.send(null);
 }
